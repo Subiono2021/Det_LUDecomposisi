f28833fe2f59dc04831bf058bc119f0d998c6873
Subiono <91346750+Subiono2021@users.noreply.github.com>
2025-04-10 06:57:51 +0700

    Update lu_determinant.py

--- lu_determinant.py
+++ lu_determinant.py
@@ -4,38 +4,22 @@ from IPython.display import display, Math
 init_printing(use_latex=True)
 
 def lu_determinant(A):
-    """
-    Menghitung determinan matriks A menggunakan LU decomposition:
-    det(A) = (-1)^k * prod(diag(U))
-    Di mana k adalah banyaknya pertukaran baris (dilihat dari matriks P)
-
-    Parameters:
-        A (sympy.Matrix): Matriks persegi
-
-    Returns:
-        det_A (sympy.Expr): Determinan dari A
-        k (int): Banyaknya pertukaran baris
-        P (sympy.Matrix): Matriks permutasi
-    """
-    # Lakukan dekomposisi LU
     L, U, perm = A.LUdecomposition()
     n = A.rows
     P = eye(n)
-    k = 0
-    perm = list(perm)
+    k = 0  # jumlah swap baris
 
-    for i in range(n):
-        while perm[i] != i:
-            j = perm[i]
+    # Fix: perm adalah daftar pasangan [i, j] yang harus ditukar
+    for swap in perm:
+        if isinstance(swap, (list, tuple)) and len(swap) == 2:
+            i, j = swap
             P.row_swap(i, j)
-            perm[i], perm[j] = perm[j], perm[i]
             k += 1
 
     sign = (-1) ** k
     diag_product = simplify(prod([U[i, i] for i in range(U.rows)]))
     det_A = simplify(sign * diag_product)
 
-    # Tampilkan hasil
     display(Math(r"A = " + latex(A)))
     display(Math(r"P = " + latex(P)))
     display(Math(r"L = " + latex(L)))
@@ -43,7 +27,7 @@ def lu_determinant(A):
     display(Math(r"LU = " + latex(L @ U)))
     display(Math(r"PA = " + latex(P @ A)))
     display(Math(r"\text{Jumlah pertukaran baris } k = " + str(k)))
-    display(Math(r"\det(A) = (-1)^k \cdot \prod u_{i,i} = {} \cdot {} = {}".format(
+    display(Math(r"\det(A) = (-1)^k \cdot \prod u_{{i,i}} = {} \cdot {} = {}".format(
         sign, latex(diag_product), latex(det_A))))
 
     return det_A, k, P
